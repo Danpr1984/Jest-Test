812b31c02c79436858906899b11e445a170acf91
Danpr1984 <daniel.porras.rosses@gmail.com>
2022-04-28 14:38:40 +0000

    Added functions and tests

--- scripts/game.js
+++ scripts/game.js
@@ -1,7 +1,27 @@
 let game = {
     currentGame: [],
+    playerMoves: [],
     score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore()
+    addTurn()
+};
 
-module.exports = { game };
+function showScore() {
+    document.getElementById ("score").innerText = game.score
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random()*4))]);
+    //showScore()
+}
+
+
+module.exports = { game, newGame, showScore, addTurn };
